f785be64f150e2065978d250f9152626b371a016
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-10-11 15:57:45 -0700

    fix: corrected incorrect 'default' attribute to 'value' attribute on interest range input
    - default value of range now set correctly
    - JS to set the value correctly no longer required - removed

--- script.js
+++ script.js
@@ -36,8 +36,3 @@ function updateRate() {
   const rateval = document.getElementById('rate').value;
   document.getElementById('rate_val').innerText = rateval + '%';
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  // Ensure the rate value is set appropriately on load
-  document.getElementById('rate').value = '10.25';
-});
